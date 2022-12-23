daa7ddf8feb13a6a84667856c64dbbf6bb6c500a
Kena Roy <75708839+Kena737@users.noreply.github.com>
2022-12-23 14:45:04 +0000

    Demo commet

--- app/ebok_card.tsx
+++ app/ebok_card.tsx
@@ -1,11 +1,202 @@
-import React from 'react'
-
-const ebok_card = () => {
+"use client";
+import Image from 'next/image';
+import Link from 'next/link';
+import { Download } from 'react-iconly';
+const Ebok_card = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className='flex flex-wrap md:justify-between justify-center'>
+      <Link href="/Courses/Science">
+        <div className='my-3 w-64 mr-5 hover:bg-[#2a2a2a] bg-[#212121] rounded-xl'>
+          <Image src="https://bit.ly/3hO2KCD" alt="Thumbnail" width={100} height={100} className="rounded-t-xl w-full h-64 object-cover object-top" />
+          <div className="p-5 rounded-b-xl">
+            <div>
+              <h2 className='text-xl font-bold'>9th Science and Technolo...</h2>
+            </div>
+            <div className='flex justify-between items-center space-x-1 mt-2'>
+              <div className='flex items-center space-x-2 dark:text-white rounded-xl font-bold h-8'>
+                <Download set="bold" primaryColor="" size={28} />
+                <span>Download</span>
+              </div>
+              <div className='p-1 px-2 rounded-md bg-green-100 text-stone-900 h-8 '>
+                <h2 className='font-bold'>Free</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+      <Link href="/Courses/Science">
+        <div className='my-3 w-64 mr-5 hover:bg-[#2a2a2a] bg-[#212121] rounded-xl'>
+          <Image src="https://bit.ly/3hO2KCD" alt="Thumbnail" width={100} height={100} className="rounded-t-xl w-full h-64 object-cover object-top" />
+          <div className="p-5 rounded-b-xl">
+            <div>
+              <h2 className='text-xl font-bold'>9th Science and Technolo...</h2>
+            </div>
+            <div className='flex justify-between items-center space-x-1 mt-2'>
+              <div className='flex items-center space-x-2 dark:text-white rounded-xl font-bold h-8'>
+                <Download set="bold" primaryColor="" size={28} />
+                <span>Download</span>
+              </div>
+              <div className='p-1 px-2 rounded-md bg-green-100 text-stone-900 h-8 '>
+                <h2 className='font-bold'>Free</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+      <Link href="/Courses/Science">
+        <div className='my-3 w-64 mr-5 hover:bg-[#2a2a2a] bg-[#212121] rounded-xl'>
+          <Image src="https://bit.ly/3hO2KCD" alt="Thumbnail" width={100} height={100} className="rounded-t-xl w-full h-64 object-cover object-top" />
+          <div className="p-5 rounded-b-xl">
+            <div>
+              <h2 className='text-xl font-bold'>9th Science and Technolo...</h2>
+            </div>
+            <div className='flex justify-between items-center space-x-1 mt-2'>
+              <div className='flex items-center space-x-2 dark:text-white rounded-xl font-bold h-8'>
+                <Download set="bold" primaryColor="" size={28} />
+                <span>Download</span>
+              </div>
+              <div className='p-1 px-2 rounded-md bg-green-100 text-stone-900 h-8 '>
+                <h2 className='font-bold'>Free</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+      <Link href="/Courses/Science">
+        <div className='my-3 w-64 mr-5 hover:bg-[#2a2a2a] bg-[#212121] rounded-xl'>
+          <Image src="https://bit.ly/3hO2KCD" alt="Thumbnail" width={100} height={100} className="rounded-t-xl w-full h-64 object-cover object-top" />
+          <div className="p-5 rounded-b-xl">
+            <div>
+              <h2 className='text-xl font-bold'>9th Science and Technolo...</h2>
+            </div>
+            <div className='flex justify-between items-center space-x-1 mt-2'>
+              <div className='flex items-center space-x-2 dark:text-white rounded-xl font-bold h-8'>
+                <Download set="bold" primaryColor="" size={28} />
+                <span>Download</span>
+              </div>
+              <div className='p-1 px-2 rounded-md bg-green-100 text-stone-900 h-8 '>
+                <h2 className='font-bold'>Free</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+      <Link href="/Courses/Science">
+        <div className='my-3 w-64 mr-5 hover:bg-[#2a2a2a] bg-[#212121] rounded-xl'>
+          <Image src="https://bit.ly/3hO2KCD" alt="Thumbnail" width={100} height={100} className="rounded-t-xl w-full h-64 object-cover object-top" />
+          <div className="p-5 rounded-b-xl">
+            <div>
+              <h2 className='text-xl font-bold'>9th Science and Technolo...</h2>
+            </div>
+            <div className='flex justify-between items-center space-x-1 mt-2'>
+              <div className='flex items-center space-x-2 dark:text-white rounded-xl font-bold h-8'>
+                <Download set="bold" primaryColor="" size={28} />
+                <span>Download</span>
+              </div>
+              <div className='p-1 px-2 rounded-md bg-green-100 text-stone-900 h-8 '>
+                <h2 className='font-bold'>Free</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+      <Link href="/Courses/Science">
+        <div className='my-3 w-64 mr-5 hover:bg-[#2a2a2a] bg-[#212121] rounded-xl'>
+          <Image src="https://bit.ly/3hO2KCD" alt="Thumbnail" width={100} height={100} className="rounded-t-xl w-full h-64 object-cover object-top" />
+          <div className="p-5 rounded-b-xl">
+            <div>
+              <h2 className='text-xl font-bold'>9th Science and Technolo...</h2>
+            </div>
+            <div className='flex justify-between items-center space-x-1 mt-2'>
+              <div className='flex items-center space-x-2 dark:text-white rounded-xl font-bold h-8'>
+                <Download set="bold" primaryColor="" size={28} />
+                <span>Download</span>
+              </div>
+              <div className='p-1 px-2 rounded-md bg-green-100 text-stone-900 h-8 '>
+                <h2 className='font-bold'>Free</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+      <Link href="/Courses/Science">
+        <div className='my-3 w-64 mr-5 hover:bg-[#2a2a2a] bg-[#212121] rounded-xl'>
+          <Image src="https://bit.ly/3hO2KCD" alt="Thumbnail" width={100} height={100} className="rounded-t-xl w-full h-64 object-cover object-top" />
+          <div className="p-5 rounded-b-xl">
+            <div>
+              <h2 className='text-xl font-bold'>9th Science and Technolo...</h2>
+            </div>
+            <div className='flex justify-between items-center space-x-1 mt-2'>
+              <div className='flex items-center space-x-2 dark:text-white rounded-xl font-bold h-8'>
+                <Download set="bold" primaryColor="" size={28} />
+                <span>Download</span>
+              </div>
+              <div className='p-1 px-2 rounded-md bg-green-100 text-stone-900 h-8 '>
+                <h2 className='font-bold'>Free</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+      <Link href="/Courses/Science">
+        <div className='my-3 w-64 mr-5 hover:bg-[#2a2a2a] bg-[#212121] rounded-xl'>
+          <Image src="https://bit.ly/3hO2KCD" alt="Thumbnail" width={100} height={100} className="rounded-t-xl w-full h-64 object-cover object-top" />
+          <div className="p-5 rounded-b-xl">
+            <div>
+              <h2 className='text-xl font-bold'>9th Science and Technolo...</h2>
+            </div>
+            <div className='flex justify-between items-center space-x-1 mt-2'>
+              <div className='flex items-center space-x-2 dark:text-white rounded-xl font-bold h-8'>
+                <Download set="bold" primaryColor="" size={28} />
+                <span>Download</span>
+              </div>
+              <div className='p-1 px-2 rounded-md bg-green-100 text-stone-900 h-8 '>
+                <h2 className='font-bold'>Free</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+      <Link href="/Courses/Science">
+        <div className='my-3 w-64 mr-5 hover:bg-[#2a2a2a] bg-[#212121] rounded-xl'>
+          <Image src="https://bit.ly/3hO2KCD" alt="Thumbnail" width={100} height={100} className="rounded-t-xl w-full h-64 object-cover object-top" />
+          <div className="p-5 rounded-b-xl">
+            <div>
+              <h2 className='text-xl font-bold'>9th Science and Technolo...</h2>
+            </div>
+            <div className='flex justify-between items-center space-x-1 mt-2'>
+              <div className='flex items-center space-x-2 dark:text-white rounded-xl font-bold h-8'>
+                <Download set="bold" primaryColor="" size={28} />
+                <span>Download</span>
+              </div>
+              <div className='p-1 px-2 rounded-md bg-green-100 text-stone-900 h-8 '>
+                <h2 className='font-bold'>Free</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+      <Link href="/Courses/Science">
+        <div className='my-3 w-64 mr-5 hover:bg-[#2a2a2a] bg-[#212121] rounded-xl'>
+          <Image src="https://bit.ly/3hO2KCD" alt="Thumbnail" width={100} height={100} className="rounded-t-xl w-full h-64 object-cover object-top" />
+          <div className="p-5 rounded-b-xl">
+            <div>
+              <h2 className='text-xl font-bold'>9th Science and Technolo...</h2>
+            </div>
+            <div className='flex justify-between items-center space-x-1 mt-2'>
+              <div className='flex items-center space-x-2 dark:text-white rounded-xl font-bold h-8'>
+                <Download set="bold" primaryColor="" size={28} />
+                <span>Download</span>
+              </div>
+              <div className='p-1 px-2 rounded-md bg-green-100 text-stone-900 h-8 '>
+                <h2 className='font-bold'>Free</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
     </div>
   )
 }
 
-export default ebok_card
+export default Ebok_card
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-"use client";
+
 import Image from 'next/image'
 import Link from 'next/link'
 import { Download } from 'react-iconly'
 import Banner from './Banner'
 import Card from './Card'
+import Ebok_card from './ebok_card'
 
 export default function Home() {
   return (
@@ -12,15 +13,6 @@ export default function Home() {
         <Banner />
       </div>
       <div className=''>
-        {/* <div className='md:my-5 flex flex-col'>
-          <div className='flex items-center space-x-2'>
-            <div className='px-2 h-8 rounded-sm bg-pink-200'></div>
-            <Link href={'/Courses'}>
-              <h1 className='text-2xl font-bold my-5 hover:text-[#e4e4e4]'>Courses</h1>
-            </Link>
-          </div>
-          <Card />
-        </div> */}
         <div className='md:my-5 flex flex-col'>
           <div className='flex items-center space-x-2'>
             <div className='px-2 h-8 rounded-sm bg-pink-200'></div>
@@ -28,27 +20,7 @@ export default function Home() {
               <h1 className='text-2xl font-bold my-5 hover:text-[#e4e4e4]'>E-Textbooks</h1>
             </Link>
           </div>
-          <div className='flex flex-wrap md:justify-between justify-center'>
-            <Link href="/Courses/Science">
-              <div className='my-3 md:w-90 hover:bg-[#2a2a2a] bg-[#212121] rounded-xl'>
-                <Image src="https://bit.ly/3hO2KCD" alt="Thumbnail" width={100} height={100} className="rounded-t-xl w-full h-96 object-cover object-top" />
-                <div className="p-5 rounded-b-xl">
-                  <div className=''>
-                    <h2 className='text-xl font-bold'>9th Science and Technolo...</h2>
-                  </div>
-                  <div className='flex justify-between items-center space-x-1'>
-                    <div className='flex items-center space-x-2'>
-                      <Download set="bold" primaryColor=""/>
-                      <span>Download</span>
-                    </div>
-                    <div className='p-1 px-2 rounded-md bg-green-100 text-stone-900 h-8 '>
-                      <h2 className='font-bold'>Free</h2>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </Link>
-          </div>
+          <Ebok_card />
         </div>
       </div>
     </main>

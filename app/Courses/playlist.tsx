b3e0060f7ea9ffb14176d9bf5415c77addff4e95
Kena Roy <75708839+Kena737@users.noreply.github.com>
2022-12-26 00:43:46 -0500

    update

--- app/Courses/playlist.tsx
+++ app/Courses/playlist.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import { Scrollbars } from 'react-custom-scrollbar  s'
+import { Scrollbars } from 'react-custom-scrollbars'
 const Playlist = () => {
     return (
         <div className=''>

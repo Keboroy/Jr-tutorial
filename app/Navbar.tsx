22ea43debf1a9d9ada44f0e2ddb8e5c9686355c7
Kena737 <singhamitsboss@gmail.com>
2022-12-26 11:47:04 -0500

    updated

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Category, Notification, Search } from 'react-iconly'
+import { Category, Notification, Search, ChevronDown } from 'react-iconly'
 
 const Navbar = () => {
   return (
@@ -8,7 +8,18 @@ const Navbar = () => {
         <h3 className="text-gray-200 text-lg">Hello,</h3>
         <h1 className="text-4xl font-bold">Ron Smith &#128075;</h1>
       </div>
-      <div className='flex space-x-2'>
+      <div className='flex space-x-2 items-center'>
+        <div>
+          <div className='rounded-full bg-[#121212] hover:bg-[#1f1f1f] px-3 space-x-2 py-1 flex items-center'>
+            <span className='text-lg font-medium'>Class 9</span>
+            <ChevronDown set="light" size={20}/>
+          </div>
+          <div>
+            <div>
+              <span></span>
+            </div>
+          </div>
+        </div>
         <div className='hidden md:block p-2 hover:border hover:border-indigo-800 rounded-xl hover:bg-indigo-900 hover:text-indigo-300 hover:bg-opacity-75'>
           <Notification set="broken" size={25} />
         </div>
